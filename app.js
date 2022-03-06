358835869cc964a139d65bcf8b629030e21ff04e
Ryan Truong <ryan.truong2304@gmail.com>
2022-03-06 16:39:37 +1100

    Added rockPaperScissorsGame function to JavaScript File. Made HTML dark.

--- app.js
+++ app.js
@@ -3,3 +3,35 @@ function computerPlay() {
 
     return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
 }
+
+function rockPaperScissorsGame(playerSelectionGame, computerSelection) {
+    playerSelectionGame = playerSelectionGame.toLowerCase();
+
+    computerSelection = computerSelection.toLowerCase();
+
+    let win = `You win. ${playerSelectionGame} beats ${computerSelection}`;
+
+    let lose = `You lose. ${computerSelection} beats ${playerSelectionGame}`;
+
+    let nothing = `Nothing happened ${playerSelectionGame} and ${computerSelection} cancel eachother`;
+
+    if(playerSelectionGame == computerSelection) {
+        return nothing;
+    } else if(playerSelectionGame == "paper" && computerSelection == "rock") {
+        return win;
+    } else if(playerSelectionGame == "paper" && computerSelection == "scissors") {
+        return lose;
+    } else if(playerSelectionGame == "rock" && computerSelection == "scissors") {
+        return win;
+    } else if(playerSelectionGame == "rock" && computerSelection == "paper") {
+        return lose;
+    } else if(playerSelectionGame == "scissors" && computerSelection == "paper") {
+        return win;
+    } else if(playerSelectionGame == "scissors" && computerSelection == "rock") {
+        return lose;
+    }
+}
+
+playerSelection = prompt("Enter Your Selection (Rock, Paper, Scissors):");
+
+rockPaperScissorsGame(playerSelection, computerPlay());
